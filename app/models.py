fc03168d0b2f3cc8b49ce9128016a10b79ac0bde
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2025-08-05 20:39:44 +0200

    Added user migration.

--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ from datetime import datetime
 
 
 class User(rx.Model, table=True):
+    __tablename__ = "users"
+
     name: str
     password: str
     created_at: datetime = rx.Field(default_factory=datetime.now())
