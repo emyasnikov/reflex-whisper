bb6802a1e457b22bd26d284e184c7db605c9d8a5
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2025-08-05 20:21:09 +0200

    Added user model.

---
+++ app/models.py
@@ -0,0 +1,12 @@
+import reflex as rx
+
+from datetime import datetime
+
+
+class User(rx.Model, table=True):
+    name: str
+    password: str
+    created_at: datetime = rx.Field(default_factory=datetime.now())
+
+    def __repr__(self):
+        return f"User: {self.name}, {self.created_at}"
