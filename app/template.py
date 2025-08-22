83af749f9bdbd7aa767529679fea37248ec96193
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2025-08-22 18:30:53 +0200

    Added decorator to template.

--- app/template.py
+++ app/template.py
@@ -6,19 +6,24 @@ from rxconfig import config
 from typing import Callable, Optional
 
 
-def template(
-    page: Callable[[], rx.Component],
-    on_load: Optional[Callable] = None,
-) -> rx.Component:
-    return rx.box(
-        navbar(
-            config.title,
-            {
-                "Home": "/",
-                "Users": "/users",
-            },
-            AuthState.on_logout,
-        ),
-        rx.container(page()),
-        on_mount=on_load,
-    )
+def template(page: Optional[Callable[[], rx.Component]] = None, *, on_load: Optional[Callable] = None) -> rx.Component:
+    def apply(page_fn: Callable[[], rx.Component]) -> rx.Component:
+        return rx.box(
+            navbar(
+                config.title,
+                {
+                    "Home": "/",
+                    "Users": "/users",
+                },
+                AuthState.on_logout,
+            ),
+            rx.container(page()),
+            on_mount=on_load or AuthState.on_load,
+        )
+    if page is not None and callable(page):
+        return apply(page)
+
+    def decorator(page_fn: Callable[[], rx.Component]):
+        return apply(page_fn)
+
+    return decorator
