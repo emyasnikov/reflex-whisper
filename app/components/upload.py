4e80f90e5c0d21c8581587a12d0d43205e78bbf4
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2025-08-23 22:26:30 +0200

    Added progress to upload.

--- app/components/upload.py
+++ app/components/upload.py
@@ -2,16 +2,27 @@ import reflex as rx
 
 
 class UploadState(rx.State):
+    current: int = 0
     files: list[str] = []
+    total: int = 0
+    uploading: bool = False
 
     @rx.event
     async def upload(self, files: list[rx.UploadFile]):
         for file in files:
             data = await file.read()
+            self.total += len(data)
             path = rx.get_upload_dir() / file.name
             with path.open("wb") as f:
                 f.write(data)
             self.files.append(file.name)
+
+    @rx.event
+    def progress(self, progress: dict):
+        self.uploading = True
+        self.current = round(progress["progress"] * 100)
+        if self.current >= 100:
+            self.uploading = False
 
 
 def upload_file():
@@ -20,11 +31,21 @@ def upload_file():
             id="upload",
             accept={
                 "audio/mpeg",
+                "audio/wav",
             },
         ),
-        rx.button(
-            "Upload",
-            on_click=UploadState.upload(rx.upload_files("upload")),
+        rx.progress(value=UploadState.current, max=100),
+        rx.cond(
+            ~UploadState.uploading,
+            rx.button(
+                "Upload",
+                on_click=UploadState.upload(
+                    rx.upload_files(
+                        upload_id="upload",
+                        on_upload_progress=UploadState.progress,
+                    ),
+                ),
+            ),
         ),
         rx.foreach(
             UploadState.files,
